9ae8c48e110fa7707cc983878d34fbf0ad67ae6f
Faraaz Jan <faraaz.jan@gmail.com>
2023-12-11 11:50:58 -0800

    uibuild

--- notesapp_deployment/index.js
+++ notesapp_deployment/index.js
@@ -40,7 +40,7 @@ app.get('/api/notes/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response, next) => {
     Note.findByIdAndDelete(request.params.id)
         .then(result => {
             response.status(204).end()
@@ -85,6 +85,8 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+    } else if(error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
     next(error)
 }
